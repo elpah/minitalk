7800a07d8342ce2034202051aca58cd5f4630aa9
El-Pachris Obeng <eobeng@c2r13s1.42wolfsburg.de>
2024-10-13 12:03:37 +0200

    added header file

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eobeng <eobeng@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 15:08:10 by eobeng            #+#    #+#             */
+/*   Updated: 2024/10/09 16:01:44 by eobeng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+# include "../printf/ft_printf.h"
+
+#endif
