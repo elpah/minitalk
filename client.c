8bf64e76585403acfe6fb8d825ed83f5b384287e
El-Pachris Obeng <eobeng@c2r13s1.42wolfsburg.de>
2024-10-13 12:02:55 +0200

    added function to convert character to binary

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: eobeng <eobeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:50:51 by eobeng            #+#    #+#             */
-/*   Updated: 2024/10/13 12:01:11 by eobeng           ###   ########.fr       */
+/*   Updated: 2024/10/13 12:01:52 by eobeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,20 @@ static void	signal_handler(int signal)
 	g_receiver = 1;
 	if (signal == SIGUSR1)
 		ft_printf("Message delivered successfully\n");
+}
+
+static void	char_to_binary(int pid, char c)
+{
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(500);
+		i--;
+	}
 }
