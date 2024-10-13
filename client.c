9600d019ec84d0eedd09cec8232c232915411dbd
El-Pachris Obeng <eobeng@c2r13s1.42wolfsburg.de>
2024-10-13 12:03:17 +0200

    client main file

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: eobeng <eobeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:50:51 by eobeng            #+#    #+#             */
-/*   Updated: 2024/10/13 12:01:52 by eobeng           ###   ########.fr       */
+/*   Updated: 2024/10/13 12:03:01 by eobeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,38 @@ static void	char_to_binary(int pid, char c)
 		usleep(500);
 		i--;
 	}
+}
+int	main(int ac, char **av)
+{
+	int					pid;
+	int					i;
+	struct sigaction	sa;
+
+	i = 0;
+	if (ac != 3)
+	{
+		ft_printf("invalid number of inputs.");
+		return (1);
+	}
+	pid = ft_atoi(av[1]);
+
+	ft_memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = &signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_printf("ERROR SIGUSR1\n");
+
+	while (av[2][i])
+	{
+		char_to_binary(pid, av[2][i]);
+		i++;
+	}
+	char_to_binary(pid, '\0');
+
+	if (!g_receiver)
+		ft_printf("ERROR SIGUSR1\n");
+
+	return (0);
 }
